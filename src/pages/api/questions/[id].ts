2153496a3c439f0f5f1fb925303fef62d9d19840
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-11-26 14:24:12 -0300

    Change folders api

--- src/pages/api/questions/[id].ts
+++ src/pages/api/questions/[id].ts
@@ -2,12 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
+  id:any
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest,res: NextApiResponse<Data>){
+  res.status(200).json({
+      id: +req.query.id, 
+      name: 'John Doe' 
+    })
 }
