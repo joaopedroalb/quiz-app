66ef3776527a9655b437a9e68c2d90a1cdaea456
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-12-08 14:35:12 -0300

    Update some stuff

--- src/pages/api/dataBaseQuestion.ts
+++ src/pages/api/dataBaseQuestion.ts
@@ -2,23 +2,29 @@ import AnswerModel from "../../model/Answer";
 import QuestionModel from "../../model/Question";
 
 const questions: QuestionModel[] =[
-    new QuestionModel(13,"Quem ganhou a copa de 2002 ?",[
+    new QuestionModel(1,"Quem ganhou a copa de 2002 ?",[
         AnswerModel.wrongAnswer("Alemanha"),
         AnswerModel.wrongAnswer("Italia"),
         AnswerModel.wrongAnswer("Espanha"),
         AnswerModel.correctAnswer("Brasil")
     ]),
-    new QuestionModel(100,"Qual jogador fez o gol da vitoria do Palmeiras na libertadores 2021 ?",[
+    new QuestionModel(2,"Qual jogador fez o gol da vitoria do Palmeiras na libertadores 2021 ?",[
         AnswerModel.wrongAnswer("Raphael Veiga"),
         AnswerModel.wrongAnswer("Gustavo Scarpa"),
         AnswerModel.wrongAnswer("Felipe Melo"),
         AnswerModel.correctAnswer("Deyverson")
     ]),
-    new QuestionModel(101,"Qual desses clubes o Garricha NUNCA jogou?",[
+    new QuestionModel(3,"Qual desses clubes o jogador Garricha NUNCA jogou?",[
         AnswerModel.wrongAnswer("Fortaleza"),
         AnswerModel.wrongAnswer("Botafogo"),
         AnswerModel.wrongAnswer("Novo Hamburgo"),
         AnswerModel.correctAnswer("Ypiranga")
+    ]),
+    new QuestionModel(4,"Qual desses clubes o jogador Wellington Paulista NUNCA jogou?",[
+        AnswerModel.wrongAnswer("Fortaleza"),
+        AnswerModel.wrongAnswer("West Ham"),
+        AnswerModel.wrongAnswer("SantoS"),
+        AnswerModel.correctAnswer("Benfica")
     ]),
 ]
 
--- src/pages/api/quiz/index.ts
+++ src/pages/api/quiz/index.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { shuffle } from '../../../functions/arrays'
 import questions from '../dataBaseQuestion'
 
 export default(req: NextApiRequest,res: NextApiResponse<any>)=>{
-    res.status(200).json(questions.map(q=>q.id))
+    const ids = questions.map(q=>q.id)
+    res.status(200).json(shuffle(ids))
 }
