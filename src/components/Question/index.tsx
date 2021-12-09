d2b298a80557be882fe262bc13f144230380df41
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-12-09 10:17:45 -0300

    Colocando a cor do leao nas letras

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -7,12 +7,19 @@ interface QuestionProps{
     value: QuestionModel
 }
 
+const letters = [
+    {value:'A',color:"#3772ff"},
+    {value:'B',color:"#ff0054"},
+    {value:'C',color:"#390099"},
+    {value:'D',color:"#ea3546"}
+]
+
 export default function Question(props:QuestionProps){
     const question = props.value
 
     function renderAnswers(){
         return question.answer.map((a,i)=>{
-            return <Answer key={i} answer={a} index={i} letter={`${i+1}`} bgColor={"#F2C866"}/>
+            return <Answer key={i} answer={a} index={i} letter={letters[i].value} bgColor={letters[i].color}/>
         })
     }
     return(
