67cd15834b79799b2a7a1b01c5901f6ae590ae62
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-12-21 09:28:17 -0300

    Create home page and move stuff to game page

---
+++ src/pages/game.tsx
@@ -0,0 +1,82 @@
+import type { NextPage } from 'next'
+import { useEffect, useRef, useState } from 'react'
+import QuestionModel from '../model/Question'
+import Quiz from '../components/Quiz'
+import { useRouter } from 'next/router' 
+
+const BASE_URL = "/api"
+
+export default function Game(){
+
+    const router = useRouter()
+
+  const [question,setQuestion] = useState<QuestionModel>()
+  const [correctQuestions,setCorrectQuestions] = useState<number>(0)
+  const [idList,setIdList] = useState<number[]>([])
+
+  async function loadIdsQuestion(){
+    const resp = await fetch(`${BASE_URL}/quiz`)
+    const ids = await resp.json()
+    console.log(ids)
+    setIdList(ids)
+  }
+
+  async function loadQuestion(id:number){
+    const resp = await fetch(`${BASE_URL}/questions/${id}`)
+    const questionJson = await resp.json();
+    const newQuestion = QuestionModel.createByObject(questionJson);
+    setQuestion(newQuestion)
+
+  }
+
+  useEffect(()=>{
+    loadIdsQuestion();
+  },[])
+
+  useEffect(()=>{
+    idList.length > 0 && loadQuestion(idList[0])
+      
+  },[idList])
+  
+  function answeredQuestion(questionAnswered:QuestionModel){
+    setQuestion(questionAnswered)
+    const correct = questionAnswered.isCorrect
+    setCorrectQuestions(correctQuestions+(correct?1:0));
+  }
+
+  function idNextQuestion(){
+    if(question===undefined)
+      return undefined
+
+    const nextIndex = idList.indexOf(question.id)+1;
+    return idList[nextIndex]
+  }
+
+  function nextStep(){
+    const nextId = idNextQuestion();
+    nextId ? goToNextQuestion(nextId):finishQuiz()
+  }
+
+  function goToNextQuestion(index:number){
+    loadQuestion(index)
+  }
+
+  function finishQuiz(){
+    router.push({
+      pathname:"/result",
+      query:{
+        total:idList.length,
+        corrects: correctQuestions
+      }
+    })
+  }
+
+    return question ?(
+        <Quiz 
+        question={question}
+        last={idNextQuestion() === undefined}
+        answeredQuestion={answeredQuestion}
+        nextStep={nextStep}
+        />
+    ):(<div></div>)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,22 @@
 import type { NextPage } from 'next'
 import { useEffect, useRef, useState } from 'react'
-import AnswerModel from '../model/Answer'
-import QuestionModel from '../model/Question'
-import styles from '../styles/Home.module.css'
-import Quiz from '../components/Quiz'
 import { useRouter } from 'next/router' 
+import MyButton from '../components/MyButton'
+import styles from '../styles/Home.module.css'
 
-
-const BASE_URL = "/api"
 
 const Home: NextPage = () => {
   const router = useRouter()
 
-  const [question,setQuestion] = useState<QuestionModel>()
-  const [correctQuestions,setCorrectQuestions] = useState<number>(0)
-  const [idList,setIdList] = useState<number[]>([])
-
-  async function loadIdsQuestion(){
-    const resp = await fetch(`${BASE_URL}/quiz`)
-    const ids = await resp.json()
-    console.log(ids)
-    setIdList(ids)
-  }
-
-  async function loadQuestion(id:number){
-    const resp = await fetch(`${BASE_URL}/questions/${id}`)
-    const questionJson = await resp.json();
-    const newQuestion = QuestionModel.createByObject(questionJson);
-    setQuestion(newQuestion)
-
-  }
-
-  useEffect(()=>{
-    loadIdsQuestion();
-  },[])
-
-  useEffect(()=>{
-    idList.length > 0 && loadQuestion(idList[0])
-      
-  },[idList])
-  
-  function answeredQuestion(questionAnswered:QuestionModel){
-    setQuestion(questionAnswered)
-    const correct = questionAnswered.isCorrect
-    setCorrectQuestions(correctQuestions+(correct?1:0));
-  }
-
-  function idNextQuestion(){
-    if(question===undefined)
-      return undefined
-
-    const nextIndex = idList.indexOf(question.id)+1;
-    return idList[nextIndex]
-  }
-
-  function nextStep(){
-    const nextId = idNextQuestion();
-    nextId ? goToNextQuestion(nextId):finishQuiz()
-  }
-
-  function goToNextQuestion(index:number){
-    loadQuestion(index)
-  }
-
-  function finishQuiz(){
-    router.push({
-      pathname:"/result",
-      query:{
-        total:idList.length,
-        corrects: correctQuestions
-      }
-    })
-  }
-
-  return question ?(
-    <Quiz 
-    question={question}
-    last={idNextQuestion() === undefined}
-    answeredQuestion={answeredQuestion}
-    nextStep={nextStep}
-    />
-  ):(<div></div>)
+  return(
+    <div className={styles.container}>
+      <header className={styles.headerContainer}>
+        <h1 className={styles.title}><strong>F</strong>UT Quiz</h1>
+        <h5>{`"Veja o quanto você sabe de futebol"`}</h5>
+      </header>
+      <MyButton url='/game' text='Jogar' />
+    </div>
+  )
 }
 
 export default Home
